955baf7f9e8303fae800a21cea6a430b1b265a28
Noah A Klem <noahaklem@gmail.com>
2021-11-17 19:39:21 -0700

    Done

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,25 @@
 // your ImageSlider code here!
+import React from "react";
+
+// function newName() {
+//   return <div>Heres some different stuff</div>
+// }
+
+export default class ImageSlider extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  
+  render () {
+    if(this.state.currentSlideIndex === 1) {
+      // return <div>{newName()}</div>
+    } else {
+      return <div>I am on slide {this.state.currentSlideIndex}</div>
+    }
+    
+  }
+}
